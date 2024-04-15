7871b887fa7769fcacfcc7a094ad5fb707e61fda
Jens Phanich <u0916721@umail.utah.edu>
2024-04-15 15:37:02 -0600

    Added better data display

--- showData.js
+++ showData.js
@@ -23,32 +23,202 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+var globalData;
 
 const db = getDatabase();
 onValue(ref(db, 'data'), (snapshot) => {
     const selectElement = document.getElementById('dataDisplay');
     console.log(snapshot.val());
-    let temp = Object.values(snapshot.val());
+    globalData = Object.values(snapshot.val());
     //console.log(temp[0].location.localCompare(temp[1].location));
-    temp.sort((a, b) => { return a.location.localeCompare(b.location) });
-    temp.forEach((item,index) => {
+    // User can sort on here, with some filters
+    globalData.sort((a, b) => { return a.location.localeCompare(b.location) });
+    globalData.forEach((item,index) => {
         const entry = document.createElement('div');
-        entry.textContent = JSON.stringify(item);
+        entry.id = "entry" + index;
         const space = document.createElement('div');
-        console.log(index);
+        console.log(item);
         if(index % 2 == 0)
         {
-        entry.className = "bg-slate-300 px-5 rounded-sm"
+        entry.className = "px-2 bg-slate-500 grid grid-rows-1 grid-flow-col gap-4 py-4"
         }
         else
         {
-            entry.className = "bg-stone-300 px-5 1 rounded-sm"
+        entry.className = "px-2 bg-stone-200 grid grid-rows-1 grid-flow-col gap-4 py-4"
         }
         selectElement.appendChild(entry);
         space.className = "py-1";
         selectElement.appendChild(space);
+        const currentEntry = document.getElementById("entry" + index);
+        const city = document.createElement('div');
+        city.className = 'bg-blue-300 py-2 rounded-xl px-2'
+        const date = document.createElement('div');
+        date.className = 'bg-green-300 py-2 rounded-xl px-2'
+        const ph = document.createElement('div');
+        ph.className = getPHColor(item.PH) + ' '+ 'py-2 rounded-xl px-2'
+        const ammonia = document.createElement('div');
+        ammonia.className = getAmmoniaColor(item.ammonia) + ' '+ 'py-2 rounded-xl px-2'
+        const nitrite = document.createElement('div');
+        nitrite.className = getNitriteColor(item.nitrite) + ' '+ 'py-2 rounded-xl px-2'
+        const nitrate = document.createElement('div');
+        nitrate.className = getNitrateColor(item.nitrate) + ' '+ 'py-2 rounded-xl px-2'
+        city.textContent = "City " + item.location;
+        date.textContent = "Date " + (new Date(item.date)).toLocaleDateString("en-US");
+        ph.textContent = "PH  " + item.PH;
+        ammonia.textContent = "Ammonia " + item.ammonia;
+        nitrite.textContent = "Nitrite " + item.nitrite;
+        nitrate.textContent = "Nitrate " + item.nitrate;
+        currentEntry.appendChild(city);
+        currentEntry.appendChild(date);
+        currentEntry.appendChild(ph);
+        currentEntry.appendChild(ammonia);
+        currentEntry.appendChild(nitrite);
+        currentEntry.appendChild(nitrate);
     })
 })
 
+function getPHColor(ph)
+{
+    if(ph < 6.0)
+    {
+        return "bg-yellow-100";
+    }
+    else if(ph <= 6.4)
+    {
+        return "bg-yellow-50";
+    }
+    else if(ph <= 7.0)
+    {
+        return "bg-emerald-100";
+    }
+    else if(ph <= 7.2)
+    {
+        return "bg-teal-100";
+    }
+    else if(ph <= 7.4)
+    {
+        return "bg-yellow-600";
+    }
+    else if(ph <= 7.8)
+    {
+        return "bg-orange-400";
+    }
+    else if(ph <= 8.0)
+    {
+        return "bg-orange-600";
+    }
+    else if(ph <= 8.2)
+    {
+        return "bg-purple-400";
+    }
+    else if(ph <= 8.4)
+    {
+        return "bg-purple-600";
+    }
+    else if(ph >= 8.6)
+    {
+        return "bg-purple-800";
+    }
+
+}
+
+function getAmmoniaColor(ammonia)
+{
+    if(ammonia == 0)
+    {
+        return "bg-yellow-400";
+    }
+    else if(ammonia == 0.25)
+    {
+        return "bg-lime-100";
+    }
+    else if(ammonia == 0.50)
+    {
+        return "bg-lime-300";
+    }
+    else if(ammonia == 1.0)
+    {
+        return "bg-lime-500";
+    }
+    else if(ammonia == 2.0)
+    {
+        return "bg-green-500";
+    }
+    else if(ammonia == 4.0)
+    {
+        return "bg-green-800";
+    }
+    else if(ammonia == 8.0)
+    {
+        return "bg-green-900";
+    }
+
+}
+
+function getNitriteColor(nitrite)
+{
+    if(nitrite == 0)
+    {
+        return "bg-sky-400";
+    }
+    else if(nitrite == 0.25)
+    {
+        return "bg-purple-300";
+    }
+    else if(nitrite == 0.50)
+    {
+        return "bg-fuchsia-300";
+    }
+    else if(nitrite == 1.0)
+    {
+        return "bg-fuchsia-500";
+    }
+    else if(nitrite == 2.0)
+    {
+        return "bg-fuchsia-700";
+    }
+    else if(nitrite == 5.0)
+    {
+        return "bg-fuchsia-900";
+    }
+    else if(nitrite == 8.0)
+    {
+        return "bg-green-900";
+    }
+
+}
+
+function getNitrateColor(nitrate)
+{
+    if(nitrate == 0)
+    {
+        return "bg-yellow-400";
+    }
+    else if(nitrate == 5)
+    {
+        return "bg-orange-300";
+    }
+    else if(nitrate == 10)
+    {
+        return "bg-orange-600";
+    }
+    else if(nitrate == 20)
+    {
+        return "bg-red-500";
+    }
+    else if(nitrate == 40)
+    {
+        return "bg-red-700";
+    }
+    else if(nitrate == 80)
+    {
+        return "bg-red-800";
+    }
+    else if(nitrate == 160)
+    {
+        return "bg-red-900";
+    }
+
+}
 
 //});
